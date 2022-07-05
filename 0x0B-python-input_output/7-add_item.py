f627d43316486a62462ec5374bb5e0da788903f5
austineoduor <austineoduor57@gmail.com>
2022-07-05 16:30:26 +0300

    file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ import json
 import sys
 
 
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = 'add_item.json'
 
