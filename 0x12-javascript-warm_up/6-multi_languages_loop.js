d7c0c03ae5417d871f115f5b33fd6ac9d0b496d2
austineoduor <austineoduor57@gmail.com>
2022-09-05 16:25:48 +0300

    js-file

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let a = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const a = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; a[i]; i++) {
   console.log(a[i]);
 }
