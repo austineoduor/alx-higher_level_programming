4ccbc8633a1c582c086b8c4901b1cb7074d6d795
austineoduor <austineoduor57@gmail.com>
2022-09-22 17:16:50 +0300

    file

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a get request to a URL and displays the body of the response
-curl -s -X GET -H "X-School-User-Id:98" "$1"
+curl -s -X GET -H "X-School-User-Id: 98" "$1"
