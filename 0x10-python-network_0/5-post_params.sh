684211adac8bd8c331d75e0e8ad349a70aad3aa5
austineoduor <austineoduor57@gmail.com>
2022-09-22 17:45:29 +0300

    file

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a POST request to a URL and displays the body of the response
-curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
