3820a2a491f149954282c9ecb768ce9c64ed6d34
austineoduor <austineoduor57@gmail.com>
2022-09-22 17:10:51 +0300

    pynet

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a get request to a URL and displays the body of the response
-curl -s "$1" -X GET -H "X-School-User-Id:98"
+curl -s -X GET -H "X-School-User-Id:98" "$1"
