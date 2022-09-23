6fb0f2e8143e121e84fbfe724a9eb8a94b2a3810
austineoduor <austineoduor57@gmail.com>
2022-09-23 17:42:33 +0300

    file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     """
         Fetching https://alx-intranet.hbtn.io/status
     """
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') 
+                                as response:
         html = response.read()
 
     print("Body response:")
