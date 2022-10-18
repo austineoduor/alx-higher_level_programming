178060f2d449b5450f804d64e5d1e3a1440fbb8c
austineoduor <austineoduor57@gmail.com>
2022-10-18 17:35:07 +0300

    file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-
+// script that displays the title of the Star Wars movie using API
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2] + '/';
-
-request.get(url, function (err, response, body) {
+const myArgs = process.argv.slice(2);
+const URLstring = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
+request(URLstring, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
