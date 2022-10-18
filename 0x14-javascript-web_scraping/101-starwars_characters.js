fe172705140711e055ba02b670cfc7ffd49a98a2
austineoduor <austineoduor57@gmail.com>
2022-10-18 17:43:19 +0300

    file

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,4 +1,4 @@
-#!/usr/bin/
+#!/usr/bin/node
 // script that prints all characters of a Star Wars movie
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
