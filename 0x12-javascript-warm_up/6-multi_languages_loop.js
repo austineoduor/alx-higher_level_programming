cd50127fbdedbc0106c795a0dd27daef29040fa8
kali <kali@kali>
2025-04-22 07:09:22 -0400

    fixed string

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const a = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const a = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; a[i]; i++) {
   console.log(a[i]);
 }
